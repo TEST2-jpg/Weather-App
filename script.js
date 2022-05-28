1165bff294bfd05092b91885504c35665e33f63a
TEST2-jpg <agapdag@gmail.com>
2022-05-28 12:50:38 +0800

    Add dom

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// class WeatherInfo {
-//     constructor(location, main, wind) {
-//         this.location = location
-//         this.main = main
-//         this.wind = wind
-//     }
-// }
 async function getAPI(location) {
     const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=2509b65fb135e05466a84e6de5fe4f84`, { mode: 'cors' });
     const data = await api.json();
@@ -12,23 +5,44 @@ async function getAPI(location) {
 }
 async function getData(infos) {
     const data = await infos;
-    const name = data.name
-    const { temp, humidity, temp_max, temp_min } = data.main
-    const { main} = data.weather[0]
-    console.log(data)
-    console.log(name,temp, humidity, temp_max, temp_min, main)
+    const name = data.name;
+    const { temp, humidity, temp_max, temp_min } = data.main;
+    const { main } = data.weather[0];
+    console.log(data);
+    console.log(name, temp, humidity, temp_max, temp_min, main);
+    domStuff(name, main, temp, humidity);
 }
 
+function domStuff(name, main, temp, humidity) {
+    const container = document.querySelector('.container');
+    const card = document.createElement('div');
+    card.classList.add('card');
+    container.appendChild(card);
+
+    const city = document.createElement('h2');
+    city.textContent = name;
+    card.appendChild(city)
+
+    const temperature = document.createElement('div');
+    temperature.textContent = Math.round(temp - 273.15) + '°C';
+    card.appendChild(temperature)
+
+    const moist = document.createElement('div');
+    moist.textContent ='Humidity: ' + humidity + '%';
+    card.appendChild(moist);
+
+    const weather = document.createElement('div');
+    weather.textContent = main;
+    card.appendChild(weather)
+
+}
 const search = document.querySelector('#search')
 search.addEventListener('submit', e => {
-    e.preventDefault()
-    const location = document.querySelector('#location').value
-    console.log(location)
-    const infos = getAPI(location)
-    getData(infos)
+    e.preventDefault();
+    const location = document.querySelector('#location').value;
+    console.log(location);
+    const infos = getAPI(location);
+    getData(infos);
+    document.querySelector('#location').value = ''
 
 })
-// // const test = prompt()
-// const infos = getAPI(test)
-
-// getData()
